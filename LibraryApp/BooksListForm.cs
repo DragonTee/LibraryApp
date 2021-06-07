614cd05c16c60dc88901df9d44b0366bd5a2a002
IvanM <manonkin123@gmail.com>
2021-06-07 20:53:02 +0300

    Implemented loading list with given row contents.

--- LibraryApp/BooksListForm.cs
+++ LibraryApp/BooksListForm.cs
@@ -1,12 +1,44 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace LibraryApp
 {
     public partial class BooksListForm : Form
     {
-        public BooksListForm()
+        private List<BooksListRow> childForms;
+        public BooksListForm(List<Book> books)
         {
             InitializeComponent();
+            foreach (var book in books)
+            {
+                var newRow = new BooksListRow(book);
+                childForms.Add(newRow);
+                
+                Control mainPanel = null;
+                foreach (Control control in Controls)
+                {
+                    if (control.Name.Equals("TablePanel"))
+                    {
+                        mainPanel = control;
+                        break;
+                    }
+                }
+                if (mainPanel == null) return;
+                
+                foreach (Control control in newRow.Controls)
+                {
+                    mainPanel.Controls.Add(control);    
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            foreach (var form in childForms)
+            {
+                form.Close();
+            }
         }
     }
 }
--- LibraryApp/BooksListLibrarianForm.cs
+++ LibraryApp/BooksListLibrarianForm.cs
@@ -1,12 +1,44 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace LibraryApp
 {
     public partial class BooksListLibrarianForm : Form
     {
-        public BooksListLibrarianForm()
+        private List<BooksListLibrarianRow> childForms;
+        public BooksListLibrarianForm(List<Book> books)
         {
             InitializeComponent();
+            foreach (var book in books)
+            {
+                var newRow = new BooksListLibrarianRow(book, this);
+                childForms.Add(newRow);
+                
+                Control mainPanel = null;
+                foreach (Control control in Controls)
+                {
+                    if (control.Name.Equals("TablePanel"))
+                    {
+                        mainPanel = control;
+                        break;
+                    }
+                }
+                if (mainPanel == null) return;
+                
+                foreach (Control control in newRow.Controls)
+                {
+                    mainPanel.Controls.Add(control);    
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            foreach (var form in childForms)
+            {
+                form.Close();
+            }
         }
     }
 }
