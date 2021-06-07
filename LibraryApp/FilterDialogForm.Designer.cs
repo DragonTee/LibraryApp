32f494a7b32e4f24d42eda9f3a53b9428e83fef0
IvanM <manonkin123@gmail.com>
2021-06-08 00:01:23 +0300

    Added filter dialog.

---
+++ LibraryApp/FilterDialogForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿using System.ComponentModel;
+
+namespace LibraryApp
+{
+    partial class FilterDialogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BookAuthor = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BookName = new System.Windows.Forms.RichTextBox();
+            this.DescendingOrder = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.BookAuthor, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BookName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.DescendingOrder, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(464, 141);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BookAuthor
+            // 
+            this.BookAuthor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookAuthor.Location = new System.Drawing.Point(95, 31);
+            this.BookAuthor.Name = "BookAuthor";
+            this.BookAuthor.Size = new System.Drawing.Size(366, 22);
+            this.BookAuthor.TabIndex = 4;
+            this.BookAuthor.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Author";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descending order";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BookName
+            // 
+            this.BookName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookName.Location = new System.Drawing.Point(95, 3);
+            this.BookName.Name = "BookName";
+            this.BookName.Size = new System.Drawing.Size(366, 22);
+            this.BookName.TabIndex = 3;
+            this.BookName.Text = "";
+            // 
+            // DescendingOrder
+            // 
+            this.DescendingOrder.Location = new System.Drawing.Point(95, 59);
+            this.DescendingOrder.Name = "DescendingOrder";
+            this.DescendingOrder.Size = new System.Drawing.Size(24, 20);
+            this.DescendingOrder.TabIndex = 5;
+            this.DescendingOrder.Text = "checkBox1";
+            this.DescendingOrder.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.OkBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(150, 0, 150, 0);
+            this.panel1.Size = new System.Drawing.Size(458, 23);
+            this.panel1.TabIndex = 6;
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OkBtn.Location = new System.Drawing.Point(150, 0);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(158, 23);
+            this.OkBtn.TabIndex = 0;
+            this.OkBtn.Text = "OK";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            // 
+            // FilterDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 141);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(480, 180);
+            this.MinimumSize = new System.Drawing.Size(480, 180);
+            this.Name = "FilterDialogForm";
+            this.Text = "FilterDialogForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.RichTextBox BookAuthor;
+        private System.Windows.Forms.RichTextBox BookName;
+        private System.Windows.Forms.CheckBox DescendingOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+        #endregion
+    }
+}
